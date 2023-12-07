91261464fc4e853dc04fd268e5319f6626eef093
Narek <narekbayanduryan16@gmail.com>
2023-12-07 21:15:07 +0300

    Bringing the POST method to a semi-working state.

--- app/main.py
+++ app/main.py
@@ -1,14 +1,7 @@
 from fastapi import FastAPI
 from fastapi import UploadFile
 
-from llama_index import (
-	VectorStoreIndex,
-	LLMPredictor,
-	ServiceContext,
-)
-from llama_index.llms import OpenAI
-from llama_index.readers.schema import Document
-from llama_index import download_loader
+from llama_index import Document, VectorStoreIndex
 
 import os
 
@@ -20,5 +13,6 @@ app = FastAPI()
 @app.post('/transfer_data_for_training')
 async def transfer_data_for_training(file: UploadFile):
 	"""We accept the .txt file, which we then send to ChatGPT for trainig."""
-	data = download_loader(file)
-	print(data)
+	text = await file.read()
+	documents = [Document(text=text)]
+	index = VectorStoreIndex.from_documents(documents)
