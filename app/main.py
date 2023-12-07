da906b5935b92fd6e073f4228e24d13294f801ba
Narek <narekbayanduryan16@gmail.com>
2023-12-07 18:37:39 +0300

    Changing the POST view.

--- app/main.py
+++ app/main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
 from fastapi import UploadFile
 
-from llama_index import SimpleDirectoryReader, GPTListIndex, readers, LLMPredictor, PromptHelper
-from langchain import OpenAI
+from llama_index import (
+	VectorStoreIndex,
+	LLMPredictor,
+	ServiceContext,
+)
+from llama_index.llms import OpenAI
+from llama_index.readers.schema import Document
+from llama_index import download_loader
 
 import os
 
@@ -14,24 +20,5 @@ app = FastAPI()
 @app.post('/transfer_data_for_training')
 async def transfer_data_for_training(file: UploadFile):
 	"""We accept the .txt file, which we then send to ChatGPT for trainig."""
-
-	max_input_size = 4096
-	num_outputs = 300
-	max_chunk_overlap = 20
-	chunk_size_limit = 600
-
-	# Define LLM.
-	llm_predictor = LLMPredictor(llm=OpenAI(temperature=0.5, model_name="text-davinci-003", max_tokens=num_outputs))
-	prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit=chunk_size_limit)
-
-	documents = SimpleDirectoryReader(file).load_data()
-
-	index = GPTSimpleVectorIndex(
-		documents, llm_predictor=llm_predictor, prompt_helper=prompt_helper
-	)
-
-	index.save_to_disk('index.json')
-
-	return index
-
-
+	data = download_loader(file)
+	print(data)
