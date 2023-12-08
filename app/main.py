04395e176cd8dd410f1cb49e3feab213670c5a4a
Narek <narekbayanduryan16@gmail.com>
2023-12-08 13:10:44 +0300

    Created another view for requests from trained ChatGPT.

--- app/main.py
+++ app/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
 from fastapi import UploadFile
 
-from llama_index import Document, VectorStoreIndex
+from pydantic import BaseModel
+
+from llama_index import (
+	Document,
+	VectorStoreIndex,
+	LLMPredictor,
+	PromptHelper,
+	GPTVectorStoreIndex,
+)
 
 import os
 
@@ -11,8 +19,28 @@ app = FastAPI()
 
 
 @app.post('/transfer_data_for_training')
-async def transfer_data_for_training(file: UploadFile):
+def transfer_data_for_training(file: UploadFile):
 	"""We accept the .txt file, which we then send to ChatGPT for trainig."""
-	text = await file.read()
-	documents = [Document(text=text)]
-	index = VectorStoreIndex.from_documents(documents)
+	text = file.file.read().decode('windows-1251')
+	optimized_text = text.encode('utf-8').decode()
+
+	documents = Document(text=optimized_text)
+	# index = GPTVectorStoreIndex.from_documents(documents)
+
+	# Save index.json file in the root folder.
+	# index.save_to_disk('index.json')
+
+	# return index
+	return documents
+
+
+@app.post('/make_request_in_chatgpt')
+def make_request_in_chatgpt(text: str):
+	"""We make a request to the trained ChatGPT."""
+	# index = GPTVectorStoreIndex.load_from_disk('index.json')
+
+	# Making a request in ChatGPT.
+	# response = index.query(text, response_mode='conpact')
+
+	# return response
+	return text
