893f06541bb05df746cd9801fea24cb3b8a84d69
Narek <narekbayanduryan16@gmail.com>
2023-12-13 17:19:54 +0300

    Tests update.

--- tests.py
+++ tests.py
@@ -5,14 +5,14 @@ URL = 'https://chatgpt-seller.onrender.com'
 
 def transfer_data_for_training_request():
 	url = URL + '/transfer_data_for_training/'
-	file = {'file': open(r'C:\Users\Нарек\Desktop\file.txt', encoding="utf-8")}
+	file = {'file': open(r'C:\Users\Нарек\Desktop\DB for GPT.txt', encoding="utf-8")}
 	resp = requests.post(url=url, files=file)
 
 	print(resp)
 
 
 def make_request_in_chatgpt():
-	text = 'как меня зовут'
+	text = 'когда работаете'
 	url = URL + '/make_request_in_chatgpt/?text=' + text
 	resp = requests.post(url=url)
 
